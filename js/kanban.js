04b404546d1fbe4b6c3f933b17ff62949f5db31d
Jonas Rosberg <rojo19@student.ju.se>
2020-06-30 14:34:23 +0200

    update

--- js/kanban.js
+++ js/kanban.js
@@ -26,7 +26,34 @@ const create_item = () => {
   
   item.addEventListener('dragend', event =>
   event.dataTransfer.clearData());
+
+  let input = document.createElement('input');
+  item.appendChild(input);
+
+  let save_btn = document.createElement('button');
+  save_btn.innerHTML = "Save";
+
+  save_btn.addEventListener('click', () => {
+    error.innerHTML = " ";
+    if (input.value !== " ") {
+      order += 1;
+      item.innerHTML = input.value;
+      adding = false;
+    } else {
+      error.innerHTML = message;
+    }
+
+    item.appendChild(save_btn);
+  })
 };
 
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener('drop', event => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text');
+    event.target.appendChild(document.getElementById(id))
+  });
+  element.addEventListener('dragover', event => {
+    event.preventDefault();
+  })
 });
