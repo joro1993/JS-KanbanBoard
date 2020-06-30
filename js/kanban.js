f81740e0287e2d2e960fa05df0d98bdb44ea1c17
Jonas Rosberg <rojo19@student.ju.se>
2020-06-30 14:12:52 +0200

    update

--- js/kanban.js
+++ js/kanban.js
@@ -21,6 +21,11 @@ const create_item = () => {
   item.classList.add('item');
   item.id = 'item-' + order;
   item.draggable = true;
+  addEventListener('dragstart', event =>
+  event.dataTransfer.setData('text', event.target.id));
+  
+  addEventListener('dragend', event =>
+  event.dataTransfer.clearData());
 };
 
 document.querySelectorAll('.drop').forEach(element => {
